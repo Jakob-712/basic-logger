a1cf37e036f309ddec6666f8e5c1fc2ddddda3b2
Jakob-712 <iakob.dalaqishvili@icloud.com>
2026-04-07 13:09:48 +0400

    Add initial code files and folders

---
+++ src/main/java/ge/edu/sangu/Launcher.java
@@ -0,0 +1,33 @@
+package ge.edu.sangu;
+
+import ge.edu.sangu.logger.ConsoleLogger;
+import ge.edu.sangu.logger.Logger;
+
+import java.util.Random;
+
+public class    Launcher {
+    private static final Logger logger = new ConsoleLogger("Launcher");
+
+    public static void main(String[] args) {
+        try {
+            logger.info("Application started");
+//            logger.info(null);
+            int sum = 0;
+            for (int i = 1; i <= 100; i++) {
+                sum += i;
+                logger.debug("Sum at iteration %d is %d", i, sum);
+            }
+
+            if (new Random().nextBoolean()) {
+                throw new Exception("Random Exception");
+            }
+
+            logger.info("Sum = %d", sum);
+            logger.info("Application finished");
+        } catch (Exception exception) {
+            exception.printStackTrace(System.out);
+        }finally {
+            logger.info("Finally Application finished");
+        }
+    }
+}
---
+++ src/main/java/ge/edu/sangu/logger/LoggerTest.java
@@ -0,0 +1,33 @@
+package ge.edu.sangu.logger;
+
+public class LoggerTest {
+    public static void main(String[] args) {
+        Logger logger = new ConsoleLogger("LoggerTest");
+
+        System.out.println("--- Testing logs ---");// ჩვეულებრივ წინადადებებს ვბეჭდავ აქ (successfully logs)
+        logger.info("This is a right message.");
+        logger.warn("This is a warning with a parameter: %s", "Check this!");
+
+        System.out.println("\n");
+        System.out.println("--- Testing exception --");
+
+        try {
+            logger.info(null);// გავუშვათ ნალ მესიჯი და ექსეფშენი დავიჭიროთ
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Caught exception: " + exception.getMessage());
+            System.out.println("Confirmation: Null message exception was handled correctly.");
+        }
+
+
+
+        try {
+            logger.error("   ");// ცარიელი (გამოტოვებები) სტრინგის გაშვება
+        } catch (IllegalArgumentException exception) {
+            System.out.println("Caught exception: " + exception.getMessage());
+            System.out.println("Confirmation: Empty or whitespace message exception was handled correctly.");
+        }
+        finally {
+            System.out.println("\n---Finally executed---");
+        }
+    }
+}
